a5f9782a84224f77fcf318e188b62d582c9be9c5
Adam Greenstein <adamgreenstein@comcast.net>
2014-07-25 19:50:08 -0400

    Added Icons

--- GroupProject/gen/com/android/groupproject/R.java
+++ GroupProject/gen/com/android/groupproject/R.java
@@ -1012,8 +1012,6 @@ containing a value of this type.
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
         public static final int ic_launcher=0x7f020057;
-        public static final int one=0x7f020058;
-        public static final int two=0x7f020059;
     }
     public static final class id {
         public static final int action_bar=0x7f05001f;
